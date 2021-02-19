1a19b38a46a9cc7e8c82571647ae3ff7c902f353
Suwat Traichiraarporn <tee_sarasas@hotmail.com>
2021-02-19 22:30:29 +0700

    base apparel second committed

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,27 +1,36 @@
 const email = document.getElementById("email")
 const form = document.getElementById("form")
+const formControl = document.querySelector(".form__control")
+const errorMessage = document.getElementsByTagName("small")
 
-function error() {
-  const formControl = input.parentElement;
-  setTimeout(() => {
-    formControl.classList.add(error);
-  }, 5000);
-
-}
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  checkInput();
+})
 
 function validateEmail(email) {
-  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(regEx.test(email.value)) {
-    return
-  } else {
-    error()
-  }
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  validateEmail(email)
-})
+function checkInput() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === '' || !validateEmail(emailValue)) {
+    setError()
+  } else {
+    email.value = '';
+  }
+  setTimeout(removeError, 3000);
+}
+
+function setError() {
+  formControl.classList.add("error")
+}
+
+function removeError() {
+  formControl.classList.remove("error")
+}
 
 
 
